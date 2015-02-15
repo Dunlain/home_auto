9224c32bf3de9ceba615fbdfaa12d3ceb1e5a963
Zach Lorusso <zlorusso@gmail.com>
2015-02-15 16:08:30 -0500

    core.routes: Added routing for home view and basic comments.

--- core/routes.py
+++ core/routes.py
@@ -1,7 +1,11 @@
 from pyramid.config import Configurator
-from .views import LoginView
+from .views import LoginView, HomeView
 
 
 def create_routes(config: Configurator):
+    # Login Page
     config.add_route(LoginView.route_name, LoginView.pattern)
     config.add_view(LoginView, route_name=LoginView.route_name)
+    # Home Page
+    config.add_route(HomeView.route_name, HomeView.pattern)
+    config.add_view(HomeView, route_name=HomeView.route_name)
