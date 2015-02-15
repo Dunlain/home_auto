b540faa9419d19e3a85a9ce3bfd943d6e93532fb
Zach Lorusso <zlorusso@gmail.com>
2015-02-15 13:47:24 -0500

    core.models: Adding List, ListCategory, and Note models to core models.

--- core/models.py
+++ core/models.py
@@ -1,11 +1,22 @@
 # Built-ins
 import datetime
 # Stack Packages
-from sqlalchemy import Column, Integer, Unicode, DateTime
+from sqlalchemy import Column, Integer, Unicode, DateTime, Enum, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 # Module Constants
 Base = declarative_base()
 
+PRIORITY_CHOICES = Enum(
+    "low",
+    "medium",
+    "high",
+    name='priority_types'
+)
+
+
+# ======
+# Models
+# ======
 
 class User(Base):
     """
@@ -18,5 +29,56 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(Unicode(255), unique=True, nullable=False)
-    password = Column(Unicode(255), nullable=False)
+    password = Column(Unicode(255), nullable=False)  # TODO: Make me secure.
     last_logged = Column(DateTime, default=datetime.datetime.utcnow)
+
+
+class Note(Base):
+    """
+    Base user-created note model.
+    Fields:
+        title: the note title
+        text: the note text
+        parent: the note that this note is in response to
+        owner: the user that created this note
+        added: the date when this note was created
+        expires: if this note will expire there will be a date
+    """
+    __tablename__ = 'notes'
+    id = Column(Integer, primary_key=True)
+    title = Column(Unicode(255), unique=True, nullable=False)
+    text = Column(Unicode(512), nullable=False)
+    parent = Column(Integer, ForeignKey(id), nullable=True, onupdate="CASCADE", ondelete='CASCADE')
+    owner = Column(Integer, ForeignKey(User.id), nullable=False, onupdate="CASCADE", ondelete='CASCADE')
+    added = Column(DateTime, default=datetime.datetime.utcnow)
+    expires = Column(DateTime, nullable=True)
+
+
+class ListCategory(Base):
+    """
+    A model by which ``List``s may be categorized.
+    Fields:
+        name: the category name
+    """
+    __tablename__ = 'list_categories'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(255), unique=True, nullable=False)
+    # TODO: More fields?
+
+
+class List(Base):
+    """
+    Base "TODO-style" list.
+    Fields:
+        name: the list name
+        category: the variety of list (ForeignKey)
+        priority: the importance of this list
+        owner: the primary user of this list
+        added: the date when this list was created
+    """
+    __tablename__ = 'lists'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(255), unique=True, nullable=False)
+    category = Column(Unicode(255), nullable=True)
+    priority = Column(PRIORITY_CHOICES, nullable=True)
+    owner = Column(Integer, ForeignKey(User.id), nullable=False, onupdate="CASCADE", ondelete='CASCADE')
