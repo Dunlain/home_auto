e0355d2f9a83878382c38c1890006cf3d69ab0c6
Zach Lorusso <zlorusso@gmail.com>
2015-02-14 23:37:38 -0500

    settings: Adding database settings and view routers to settings.py

--- __main__.py
+++ __main__.py
@@ -1,13 +1,11 @@
+# Stack Packages
 from wsgiref.simple_server import make_server
-
+# Home Automation Packages
 import settings
-from core.routes import create_routes as create_core_routes
 
 
 if __name__ == '__main__':
     config = settings.create_configuration()
-    # Register views for requests to server
-    create_core_routes(config)
     # Create WSGI App
     app = config.make_wsgi_app()
     # Launch server
--- settings.py
+++ settings.py
@@ -1,11 +1,75 @@
+# Stack Packages
 import pyramid_jinja2
 from pyramid.config import Configurator
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm.session import sessionmaker
+# Home Automation Packages
+import core.routes
 
+# Default Server Hosts
 SERVER_HOST = ('0.0.0.0', 8080)
+DB_HOST = ('0.0.0.0', 5432)
+
+# Default Database Settings
+DB_SETTINGS = {
+
+}
+
+# Server Default Template Renderer
+TEMPLATE_RENDERER = pyramid_jinja2
+
+# Rate at which templates are refreshed (seconds)
+MAX_CACHE_AGE = 3600
+
+# View modules
+VIEW_ROUTERS = (
+    core.routes,  # Core views
+)
+
+
+def db_factory(request):
+    """
+    Create a database session upon a successful request to the home automation server.
+    This allows an SQLAlchemy session to be available in view code as ``request.db`` or ``config.registry.dbmaker()``.
+
+    :param request: an HTTP request object
+    :return: a database session
+    """
+    maker = request.registry.dbmaker
+    session = maker()
+
+    def cleanup(request):
+        if request.exception is not None:
+            session.rollback()
+        else:
+            session.commit()
+        session.close()
+    request.add_finished_callback(cleanup)
+
+    return session
 
 
 def create_configuration():
+    """
+    Create the app configuration based upon the settings.py file
+    :return: The app Configurator object.
+    """
     config = Configurator()
-    config.add_static_view('static', 'static/', cache_max_age=3600)
-    config.include(pyramid_jinja2)
-    config.add_renderer('.html', factory=pyramid_jinja2.renderer_factory)
+
+    # Add Database Engine
+    engine = create_engine(DB_HOST, DB_SETTINGS)
+    config.registry.dbmaker = sessionmaker(bind=engine)
+    config.add_request_method(db_factory, reify=True)
+
+    # Add Static Files
+    config.add_static_view('static', 'static/', cache_max_age=MAX_CACHE_AGE)
+
+    # Add Template Renderer
+    config.include(TEMPLATE_RENDERER)
+    config.add_renderer('.html', factory=TEMPLATE_RENDERER.renderer_factory)
+
+    # Register views for requests to server
+    for router_module in VIEW_ROUTERS:
+        router_module.create_routes(config)
+
+    return config
