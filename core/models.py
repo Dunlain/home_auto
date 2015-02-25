944b66cbfd6b48987e63ed7a6501fd6b843878d2
Zach Lorusso <zlorusso@gmail.com>
2015-02-24 21:08:43 -0500

    core.models: Added email field to user model.

--- core/models.py
+++ core/models.py
@@ -29,6 +29,7 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(Unicode(255), unique=True, nullable=False)
+    email = Column(Unicode(255), unique=True, nullable=False)
     password = Column(Unicode(255), nullable=False)  # TODO: Make me secure.
     last_logged = Column(DateTime, default=datetime.datetime.utcnow)
 
