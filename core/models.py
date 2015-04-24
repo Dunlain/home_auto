0c6e7691f8464fbff1d299a54da0de4869c5c569
Dunlain <zlorusso@gmail.com>
2015-04-23 22:34:12 -0400

    core.models: removing extraneous SQL structures.

--- core/models.py
+++ core/models.py
@@ -3,14 +3,9 @@ import datetime
 # Stack Packages
 from sqlalchemy import Column, Integer, Unicode, DateTime, Enum, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-# Module Constants
-from sqlalchemy.orm.scoping import scoped_session
-from sqlalchemy.orm.session import sessionmaker
-from zope.sqlalchemy.datamanager import ZopeTransactionExtension
+
 
 Base = declarative_base()
-DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
-
 PRIORITY_CHOICES = Enum(
     "low",
     "medium",
